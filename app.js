2415661ad9305684bb432809ba4bd5ef7eda3075
MasoumeVesali <masoumevsl198@gmail.com>
2021-05-09 18:50:13 +0430

    add
    Number

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// string
 let firstName = 'masoume'
 let lastName = 'vesali'
 let fullName = firstName + ' ' + lastName
 
-console.log(fullName)
+// Number
+let num1 = 12
+let num2 = 20
+let sum = num1 + num2
+
+console.log(sum)
