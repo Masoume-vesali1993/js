3fbcad833322fae121a11aa58ebf89e958c8387e
MasoumeVesali <masoumevsl198@gmail.com>
2021-05-09 18:51:43 +0430

    number
    ather part

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let fullName = firstName + ' ' + lastName
 // Number
 let num1 = 12
 let num2 = 20
-let sum = num1 + num2
+// let sum = num1 + num2
+// let sum = num1 / num2
+let sum = num1 * num2
 
 console.log(sum)
