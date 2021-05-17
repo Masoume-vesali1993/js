5221ef90dc45da3b1bb6ca39b651f28fb182c8cf
MasoumeVesali <masoumevsl198@gmail.com>
2021-05-18 01:01:13 +0430

    use if in if

--- app.js
+++ app.js
@@ -68,3 +68,13 @@ if (true) {
 
 console.log(firstName)
 
+// next
+let name = 'test1'
+ if(true){
+    let name = 'test2'
+    if(true){
+        let name = 'test3'
+        console.log(name)
+    }
+ }
+
